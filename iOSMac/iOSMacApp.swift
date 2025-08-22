b157b29c725eaf5c4c29d9eead30d55fc783b133
Stossy11 <stossy11@stossy11.com>
2025-08-22 11:35:15 +1000

    Initial Commit

---
+++ iOSMac/iOSMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMacApp.swift
+//  iOSMac
+//
+//  Created by Stossy11 on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
